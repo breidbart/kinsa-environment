49e494e07dd93e11f63c01194b59256ae9598549
Joe Bergantine <jbergantine@gmail.com>
2014-05-09 09:22:08 -0600

    fixed the not_if checks for installing aliases for shell_plus and runserver_plus

--- my-recipes/cookbooks/bootstrap/recipes/shellplus.rb
+++ my-recipes/cookbooks/bootstrap/recipes/shellplus.rb
@@ -6,5 +6,5 @@ bash "override the sh alias to use bpython" do
   code <<-EOH
     echo "alias sh='python manage.py shell_plus --bpython'" >> /home/vagrant/.bash_profile
   EOH
-  not_if "cat /home/vagrant/.bash_profile | grep \"alias sh='python manage.py shell_plus --use-bpython'\""
+  not_if "cat /home/vagrant/.bash_profile | grep shell_plus"
 end
