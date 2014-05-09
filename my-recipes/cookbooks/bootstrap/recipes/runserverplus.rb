be8d08f1496c20a105e95d17a0ae00b449958949
Joe Bergantine <jbergantine@gmail.com>
2014-05-09 17:13:37 -0600

    Updated runserverplus recipe
    * Fixed the grep expression to search for the correct command
    * Simplified the grep expression

--- my-recipes/cookbooks/bootstrap/recipes/runserverplus.rb
+++ my-recipes/cookbooks/bootstrap/recipes/runserverplus.rb
@@ -6,5 +6,5 @@ bash "override the rs alias to use runserver_plus" do
   code <<-EOH
     echo "alias rs='python manage.py runserver_plus [::]:8000'" >> /home/vagrant/.bash_profile
   EOH
-  not_if "cat /home/vagrant/.bash_profile | grep \"alias sh='python manage.py shell_plus --use-bpython'\""
+  not_if "cat /home/vagrant/.bash_profile | grep runserver_plus"
 end
